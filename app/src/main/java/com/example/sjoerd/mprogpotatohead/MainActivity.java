89a4ad1279619d84a1d3dbaa0f5597529489a48a
Sjoerd20 <sjoerd.terpstra@outlook.com>
2018-02-08 13:29:12 +0100

    Added checkboxes with constraints and lined out, Body parts invisibele

--- app/src/main/java/com/example/sjoerd/mprogpotatohead/MainActivity.java
+++ app/src/main/java/com/example/sjoerd/mprogpotatohead/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void checkClicked(View view) {
+    public void checkClicked(View v) {
         Log.d("potato", "checkClicked");
+        CheckBox checkbox = (CheckBox) v;
+        if (checkbox.isChecked()) {
+
+        }
     }
 }
