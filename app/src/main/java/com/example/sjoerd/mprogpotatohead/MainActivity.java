933b35900d700159b0e42694a746dff4a8609ede
Sjoerd20 <sjoerd.terpstra@outlook.com>
2018-02-08 15:59:58 +0100

    Optimalization of checkClicked function with a loop

--- app/src/main/java/com/example/sjoerd/mprogpotatohead/MainActivity.java
+++ app/src/main/java/com/example/sjoerd/mprogpotatohead/MainActivity.java
@@ -25,54 +25,16 @@ public class MainActivity extends AppCompatActivity {
         CheckBox checkbox = (CheckBox) v;
         Log.d("potato", "checkClicked");
 
-        for (int i = 0; i < 8; i++) {
+        // Loops over all body parts to change visibility of the checked body part
+        for (int i = 0; i < 10; i++) {
             if (checkbox.getText().toString().equals(bodypartsArray[i])) {
                 ImageView image = (ImageView) findViewById(idArray[i]);
                 setVisibilityimage(checkbox, image, v);
                 break;
             }
         }
-        if (checkbox.getText().toString().equals("Arms")) {
-            ImageView imageArms = (ImageView) findViewById(R.id.armsView);
-            setVisibilityimage(checkbox, imageArms, v);
-        }
-        else if (checkbox.getText().toString().equals("Hat")) {
-            ImageView imageHat = (ImageView) findViewById(R.id.hatView);
-            setVisibilityimage(checkbox, imageHat, v);
-        }
-        else if (checkbox.getText().toString().equals("Mustache")) {
-            ImageView imageMustache = (ImageView) findViewById(R.id.mustacheView);
-            setVisibilityimage(checkbox, imageMustache, v);
-        }
-        else if (checkbox.getText().toString().equals("Shoes")) {
-            ImageView imageShoes = (ImageView) findViewById(R.id.shoesView);
-            setVisibilityimage(checkbox, imageShoes, v);
-        }
-        else if (checkbox.getText().toString().equals("Eyebrows")) {
-            ImageView imageEyebrows = (ImageView) findViewById(R.id.eyebrowsView);
-            setVisibilityimage(checkbox, imageEyebrows, v);
-        }
-        else if (checkbox.getText().toString().equals("Eyes")) {
-            ImageView imageEyes = (ImageView) findViewById(R.id.eyesView);
-            setVisibilityimage(checkbox, imageEyes, v);
-        }
-        else if (checkbox.getText().toString().equals("Ears")) {
-            ImageView imageEars = (ImageView) findViewById(R.id.earsView);
-            setVisibilityimage(checkbox, imageEars, v);
-        }
-        else if (checkbox.getText().toString().equals("Glasses")) {
-            ImageView imageGlasses = (ImageView) findViewById(R.id.glassesView);
-            setVisibilityimage(checkbox, imageGlasses, v);
-        }
-        else if (checkbox.getText().toString().equals("Mouth")) {
-            ImageView imageMouth = (ImageView) findViewById(R.id.mouthView);
-            setVisibilityimage(checkbox, imageMouth, v);
-        }
-        else if (checkbox.getText().toString().equals("Nose")) {
-            ImageView imageNose = (ImageView) findViewById(R.id.noseView);
-            setVisibilityimage(checkbox, imageNose, v);
-        }
-    }
+
+}
 
     // Method to change the visibility of the given image
     private void setVisibilityimage(CheckBox checkbox, ImageView image, View v) {
