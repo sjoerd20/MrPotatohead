1480523fd231b348cefa08e7307edcc575096d3e
sjoerd20 <sjoerd.terpstra@outlook.com>
2018-02-09 16:12:18 +0100

    InstanceState geïmplementeerd

--- app/src/main/java/com/example/sjoerd/mprogpotatohead/MainActivity.java
+++ app/src/main/java/com/example/sjoerd/mprogpotatohead/MainActivity.java
@@ -9,19 +9,50 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] bodypartsArray = new String[]{"Arms", "Hat", "Mustache", "Shoes", "Eyebrows",
+            "Eyes", "Ears", "Glasses", "Mouth", "Nose"};
+    int[] idArray = new int[]{R.id.armsView, R.id.hatView, R.id.mustacheView, R.id.shoesView,
+            R.id.eyebrowsView, R.id.eyesView, R.id.earsView,
+            R.id.glassesView, R.id.mouthView, R.id.noseView};
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("onCreate", "Creating");
+        if (savedInstanceState != null) {
+            Log.d("onCreate", "savedInstanceState not null");
+            for (int i = 0; i < 10; i++) {
+                ImageView image = (ImageView) findViewById(idArray[i]);
+                if (image != null) {
+                    Log.d("onCreate", "image not null");
+                    int visible = savedInstanceState.getInt(bodypartsArray[i]);
+                    image.setVisibility(visible);
+                }
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Loops over all body parts to change visibility of the checked body part
+        for (int i = 0; i < 10; i++) {
+            Log.d("onSave", "Saving");
+            ImageView image = (ImageView) findViewById(idArray[i]);
+            outState.putInt(bodypartsArray[i], image.getVisibility());
+        }
     }
 
     // Method to check which checkbox is clicked and calls setVisibilityimage to change visibility
     public void checkClicked(View v) {
-        String[] bodypartsArray = new String[]{"Arms", "Hat", "Mustache", "Shoes", "Eyebrows",
-                                                "Eyes", "Ears", "Glasses", "Mouth", "Nose"};
-        int[] idArray = new int[]{R.id.armsView, R.id.hatView, R.id.mustacheView, R.id.shoesView,
-                                    R.id.eyebrowsView, R.id.eyesView, R.id.earsView,
-                                    R.id.glassesView, R.id.mouthView, R.id.noseView};
+
         CheckBox checkbox = (CheckBox) v;
         Log.d("potato", "checkClicked");
 
@@ -45,4 +76,5 @@ public class MainActivity extends AppCompatActivity {
             image.setVisibility(v.INVISIBLE);
         }
     }
+
 }
